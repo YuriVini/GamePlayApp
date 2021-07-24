a899684639eb79a9bf37bb9f2ccfe96368d254a5
YuriVini <yuri.ybgt.yfo@gmail.com>
2021-07-24 09:40:28 -0300

    Config Svg and some dependencies

---
+++ metro.config.js
@@ -0,0 +1,21 @@
+// expo v40:
+const { getDefaultConfig } = require("@expo/metro-config");
+
+// expo v41: 
+// remove the @ (see: https://blog.expo.io/expo-sdk-41-12cc5232f2ef)
+const { getDefaultConfig } = require("expo/metro-config");
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts }
+  } = await getDefaultConfig(__dirname);
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve("react-native-svg-transformer")
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== "svg"),
+      sourceExts: [...sourceExts, "svg"]
+    }
+  };
+})();
