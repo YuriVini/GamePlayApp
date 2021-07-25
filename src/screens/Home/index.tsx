aa390804c8e093f82ce3d60ea2c569e5ff950c06
YuriVini <yuri.ybgt.yfo@gmail.com>
2021-07-25 10:48:20 -0300

    Updating Styles

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -69,25 +69,25 @@ export function Home() {
                 setCategory={handleCategorySelect}
             />
 
-            <View style={styles.content}>
-                <ListHeader 
-                    title="Partidas Agendadas"
-                    subtitle="Total 6"
-                />
-                <FlatList 
-                    data={appointments}
-                    keyExtractor={ item=> item.id}
-                    renderItem={({ item }) => (
+            <ListHeader 
+                title="Partidas Agendadas"
+                subtitle="Total 6"
+            />
+                   
+            <FlatList 
+                data={appointments}
+                keyExtractor={ item=> item.id}
+                renderItem={({ item }) => (
                     <Appointment 
                         data={item} 
                         onPress={handleAppointmentDetails}
                     />
-                    )}
-                    ItemSeparatorComponent={() => <ListDivider />}
-                    style={styles.matches}
-                    showsVerticalScrollIndicator={false}
-                />
-            </View>       
+                )}
+                ItemSeparatorComponent={() => <ListDivider />}
+                contentContainerStyle={{paddingBottom: 69}}
+                style={styles.matches}
+                showsVerticalScrollIndicator={false}
+            />
         </Background>
     );
 }
