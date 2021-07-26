995eb96c37017cfc9dbe2a7f9d3e0105220efd11
YuriVini <yuri.ybgt.yfo@gmail.com>
2021-07-26 11:31:35 -0300

    Taking user and avatar through api

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -3,13 +3,16 @@ import {
     View,
     Text,
 } from 'react-native';
+import { useAuth } from '../../hooks/auth';
 import { Avatar } from '../Avatar';
 import { styles } from './styles';
 
 export function Profile(){
+    const { user } = useAuth();
+
     return (
         <View style={styles.container}>
-            <Avatar urlImage="https://github.com/YuriVini.png"/>
+            <Avatar urlImage={user.avatar}/>
             <View>
                 <View style={styles.user}>
                     <Text style={styles.greeting}>
@@ -17,7 +20,7 @@ export function Profile(){
                     </Text>
 
                     <Text style={styles.username}>
-                        Yuri
+                        {user.firstName}
                     </Text>
                 </View>
                 <Text style={styles.message}>
