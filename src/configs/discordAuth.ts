c3389a071338531c6332ed8f77af19897960c4ae
YuriVini <yuri.ybgt.yfo@gmail.com>
2021-07-26 10:48:02 -0300

    API settings

---
+++ src/configs/discordAuth.ts
@@ -0,0 +1,14 @@
+const REDIRECT_URI = 'https://auth.expo.io/@yuriviniciu00/gameplay';
+const SCOPE = 'identify%20email%20connections%20guilds';
+const RESPONSE_TYPE = 'token';
+const CLIENT_ID = '868876141715415050';
+const CDN_IMAGE = 'https://cdn.discordapp.com';
+
+export {
+    REDIRECT_URI,
+    SCOPE,
+    RESPONSE_TYPE,
+    CLIENT_ID,
+    CDN_IMAGE,
+}
+
